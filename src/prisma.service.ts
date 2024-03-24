cd1928db79d33d6917d1436ca98239101dca66e2
renhubgit <renik2001@gmail.com>
2024-03-24 14:52:51 +0100

    prisma service

---
+++ src/prisma.service.ts
@@ -0,0 +1,10 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  //  Method that connects to the database when the module is initialized using await this.$connect().
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
