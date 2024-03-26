ca4a3d8842178e9bc958314e674878b1876948fc
renhubgit <renik2001@gmail.com>
2024-03-26 20:50:07 +0100

    binded ValidationPipe at the application level

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ValidationPipe ensures that all endpoints are protected from receiving incorrect data
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
