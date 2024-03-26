e7bcf270e9e7f8fb2fbfa29cf51c5671b97a6106
renhubgit <renik2001@gmail.com>
2024-03-26 20:47:36 +0100

    added user.dto.ts file for validation

---
+++ src/user/dto/user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  readonly username: string;
+  @IsNotEmpty()
+  readonly password: string;
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+}
