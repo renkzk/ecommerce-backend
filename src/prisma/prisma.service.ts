a6fdedd602465308bb8dcf5459ee56362c4427dc
renhubgit <renik2001@gmail.com>
2024-04-04 22:13:55 +0200

    added logs to prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,12 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   //  Method that connects to the database when the module is initialized using await this.$connect().
   async onModuleInit() {
-    await this.$connect();
+    await this.$connect()
+      .then(() => console.log('Database connected'))
+      .catch((error) => console.log('Database connection failed'))
   }
 }
