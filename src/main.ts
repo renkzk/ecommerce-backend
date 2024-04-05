3cb8eff2776594b7f6ebe09492c67504454bd975
renhubgit <renik2001@gmail.com>
2024-04-05 21:56:23 +0200

    added comments

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   // ValidationPipe ensures that all endpoints are protected from receiving incorrect data
+  // This enables us to use DTO declarations to validate incoming data
+  // For more information, see https://docs.nestjs.com/techniques/validation
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
