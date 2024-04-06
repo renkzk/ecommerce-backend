3226ee50e542ca4b06efd356d146bfd70372cbcd
renhubgit <renik2001@gmail.com>
2024-04-06 18:43:10 +0200

    swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
   // This enables us to use DTO declarations to validate incoming data
   // For more information, see https://docs.nestjs.com/techniques/validation
   app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('E-Commerce API Documentation for E-Commerce App')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
