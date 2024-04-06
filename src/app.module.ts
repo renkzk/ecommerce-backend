6ee054cc8f33ae7557c6ae498bac9edf61e30112
renhubgit <renik2001@gmail.com>
2024-04-06 18:08:33 +0200

    deleted unused app controller and service files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [ProductsModule, UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
