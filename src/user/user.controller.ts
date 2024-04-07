d25542e8a3599a55772bbf9d88b5318d01376bb1
renhubgit <renik2001@gmail.com>
2024-04-07 18:23:03 +0200

    user controller & service refactoring (decorators, typings, comments)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,35 +1,64 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserResponseType } from 'src/types/user.type';
 import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UserResponseEntity } from './entities/user.entity';
+import { plainToClass } from 'class-transformer';
 
-@Controller('api/v1/users')
+@ApiTags('Users')
+@Controller('users')
 export class UserController {
-  constructor(private readonly UserService: UserService) {}
+  constructor(private readonly userService: UserService) {}
+
+  // About "@Api"
+  // @Api is a decorator from "@nestjs/swagger" library,
+  // it allows us to customize the response for an endpoint so that we can see it in Swagger UI Documentation
+
+  // About "ParseIntPipe"
+  // ParseIntPipe automatically converts the incoming parameter to an integer type
+
+  // About "plainToClass"
+  // When we return a "User" object in the response
+  // we need to use "plainToClass" with "UserResponseEntity" class-transformer,
+  // this removes the "password" field from the response.
 
   @Get()
-  async getAll(): Promise<UserResponseType[]> {
-    return this.UserService.getAll();
+  @ApiOperation({ summary: 'Get all Users' })
+  @ApiOkResponse({ type: [UserResponseEntity] })
+  async getAll(): Promise<UserResponseEntity[]> {
+    const users = await this.userService.getAll();
+    return users.map((user) => plainToClass(UserResponseEntity, user));
   }
 
   @Get(':id')
-  async get(@Param('id', ParseIntPipe) id: number): Promise<UserResponseType> {
-    //ParseIntPipe automatically converts the incoming parameter to an integer type
-    return this.UserService.get(id);
+  @ApiOperation({ summary: 'Get a single User by ID' })
+  @ApiOkResponse({ type: UserResponseEntity })
+  async get(@Param('id', ParseIntPipe) id: number): Promise<UserResponseEntity> {
+    const user = await this.userService.get(id);
+    return plainToClass(UserResponseEntity, user);
   }
 
   @Post()
-  async create(@Body() body: CreateUserDto): Promise<UserResponseType> {
-    return this.UserService.create(body);
+  @ApiOperation({ summary: 'Create a single User' })
+  @ApiCreatedResponse({ type: UserResponseEntity })
+  async create(@Body() body: CreateUserDto): Promise<UserResponseEntity> {
+    const createdUser = await this.userService.create(body);
+    return plainToClass(UserResponseEntity, createdUser);
   }
 
   @Patch(':id')
-  async update(@Param('id', ParseIntPipe) id: number, @Body() user: UpdateUserDto): Promise<UserResponseType> {
-    return this.UserService.update(id, user);
+  @ApiOperation({ summary: 'Update a single User by ID' })
+  @ApiOkResponse({ type: UserResponseEntity })
+  async update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateUserDto): Promise<UserResponseEntity> {
+    const updatedUser = await this.userService.update(id, body);
+    return plainToClass(UserResponseEntity, updatedUser);
   }
 
   @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number): Promise<UserResponseType> {
-    return this.UserService.delete(Number(id));
+  @ApiOperation({ summary: 'Delete a single User by ID' })
+  @ApiOkResponse({ type: UserResponseEntity })
+  async delete(@Param('id', ParseIntPipe) id: number): Promise<UserResponseEntity> {
+    const deletedUser = await this.userService.delete(id);
+    return plainToClass(UserResponseEntity, deletedUser);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,55 +1,53 @@
 import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
-import { UserResponseType } from 'src/types/user.type';
 import { Prisma, User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async getAll(): Promise<UserResponseType[]> {
-    const users = await this.prisma.user.findMany();
-    return users.map((user) => this.buildUserResponse(user));
+  async getAll(): Promise<User[]> {
+    const foundUsers = await this.prisma.user.findMany();
+    return foundUsers;
   }
 
-  async get(id: number): Promise<UserResponseType> {
-    const user = await this.prisma.user.findUnique({ where: { id } });
-    if (!user) {
+  async get(id: number): Promise<User> {
+    const foundUser = await this.prisma.user.findUnique({ where: { id } });
+    if (!foundUser) {
       throw new NotFoundException('User not found');
     }
-    // return new UserResponseEntity(user);
-    return this.buildUserResponse(user);
+    return foundUser;
   }
 
-  async create(user: Prisma.UserCreateInput): Promise<UserResponseType> {
+  async create(body: Prisma.UserCreateInput): Promise<User> {
     // Transactions help ensure that a series of operations are either all completed successfully or rolled back in case of any failure.
     return await this.prisma.$transaction(async (prismaTransaction) => {
       // Check if the user already exists by username or email
-      const existingUser = await prismaTransaction.user.findFirst({
+      const foundUser = await prismaTransaction.user.findFirst({
         where: {
-          OR: [{ username: user.username }, { email: user.email }],
+          OR: [{ username: body.username }, { email: body.email }],
         },
       });
-      if (existingUser) {
+      if (foundUser) {
         throw new ConflictException('Username or email already exists');
       }
       // Hash the password before saving it to the database
-      const hashedPassword = await bcrypt.hash(user.password, 10);
-      const userWithHashedPassword = { ...user, password: hashedPassword };
+      const hashedPassword = await bcrypt.hash(body.password, 10);
+      const userWithHashedPassword = { ...body, password: hashedPassword };
 
       // Create the user in the database
       const newUser = await prismaTransaction.user.create({ data: userWithHashedPassword });
 
       // Return the created user
-      return this.buildUserResponse(newUser);
+      return newUser;
     });
   }
 
-  async update(id: number, body: Prisma.UserUpdateInput): Promise<UserResponseType> {
+  async update(id: number, body: Prisma.UserUpdateInput): Promise<User> {
     // Check if the user exists
-    const existingUser = await this.prisma.user.findFirst({ where: { id } });
-    if (!existingUser) {
+    const foundUser = await this.prisma.user.findFirst({ where: { id } });
+    if (!foundUser) {
       throw new NotFoundException('User not found');
     }
 
@@ -57,15 +55,14 @@ export class UserService {
       where: { id },
       data: body,
     });
-    return this.buildUserResponse(updatedUser);
+    return updatedUser;
   }
 
-  async delete(id: number): Promise<UserResponseType> {
-    return this.prisma.user.delete({ where: { id } });
-  }
-
-  buildUserResponse(user: User): UserResponseType {
-    delete user.password;
-    return user;
+  async delete(id: number): Promise<User> {
+    const foundUser = await this.prisma.user.delete({ where: { id } });
+    if (!foundUser) {
+      throw new NotFoundException('User not found');
+    }
+    return foundUser;
   }
 }
