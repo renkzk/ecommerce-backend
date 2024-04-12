acaa9d26d0af24e096e11c41e0ed4a804828ed4f
renhubgit <renik2001@gmail.com>
2024-04-12 19:48:22 +0200

    added "error-code" enum

---
+++ src/shared/enums/error-code.enum.ts
@@ -0,0 +1,6 @@
+export enum ErrorCode {
+  Unauthorized = 'unauthorized',
+  UserNotFound = 'user-not-found',
+  UserAlreadyExists = 'user-already-exists',
+  InvalidCredentials = 'invalid-credentials',
+}
